7fc5c787445b2b647cac657ec1ab04dd8e6071cd
Hassan60200 <derkaouihassan19@gmail.com>
2020-08-15 18:39:07 +0200

    serializer

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -4,24 +4,19 @@ namespace App\Controller;
 
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiPostController extends AbstractController
 {
     /**
      * @Route("/api/post", name="api_post_index", methods={"GET"})
      */
-    public function index(PostRepository $postRepository, NormalizerInterface $normalizer)
+    public function index(PostRepository $postRepository, NormalizerInterface $normalizer, SerializerInterface $serializer)
     {
-        $post = $postRepository->findAll();
-
-        $postNormalises = $normalizer->normalize($post, null, ['groups' => 'post:read']);
-
-        dd($postNormalises);
-
-        return $this->render('api_post/index.html.twig', [
-            'controller_name' => 'ApiPostController',
-        ]);
+        return $response = $this->json($postRepository->findAll(), 200, [], ['groups' => 'post:read']);
     }
 }
